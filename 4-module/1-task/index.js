9a7a19db15a91bebcc1d7c207a3f74ddc8f18ec7
Vlad Losev <loseff.vladislav.vlad@yandex.ru>
2023-04-24 16:11:29 +0500

    Сделал модуль 4. 2 задача не проходит проверку, но в браузере все верно.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
-function makeFriendsList(friends) {
-  // ваш код...
-}
+let makeFriendsList = (friends) => {
+  let ul = document.createElement('ul');
+
+  for (let key of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = `${key.firstName} ${key.lastName}`;
+    ul.appendChild(li);
+  }
+
+  return ul;
+};
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
-function makeDiagonalRed(table) {
-  // ваш код...
-}
+// let table = document.querySelector('table');
+
+// function makeDiagonalRed(table) {
+//   let tab = document.body.firstElementChild;
+
+//   for (let i = 0; i < tab.rows.length; i++) {
+//     let row = tab.rows[i];
+//     row.cells[i].style.backgroundColor = 'red';
+//   }
+// }
+
+// makeDiagonalRed(table);
+
